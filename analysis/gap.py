6a9e73231b4461aa8f438904612e8f29c681cbc7
Michal Migurski <mike@teczno.com>
2017-02-26 15:45:41 -0800

    Juggling variable names to for clarity

--- analysis/gap.py
+++ analysis/gap.py
@@ -7,32 +7,31 @@ def main(plan_path):
     plan_ds = ogr.Open(plan_path)
     plan_lyr = plan_ds.GetLayer(0)
     
-    votes_total, wasted_red, wasted_blue = 0, 0, 0
+    election_votes, wasted_red, wasted_blue = 0, 0, 0
     
     for feature in plan_lyr:
         red_votes, blue_votes = feature.GetField('pres_red'), feature.GetField('pres_blue')
         district_votes = red_votes + blue_votes
-        
+        election_votes += district_votes
+        win_threshold = district_votes / 2
+    
         if red_votes > blue_votes:
-            wasted_blue += blue_votes
-            wasted_red += red_votes - (district_votes / 2)
+            wasted_red += red_votes - win_threshold # surplus
+            wasted_blue += blue_votes # loser
         elif blue_votes > red_votes:
-            wasted_red += red_votes
-            wasted_blue += blue_votes - (district_votes / 2)
+            wasted_blue += blue_votes - win_threshold # surplus
+            wasted_red += red_votes # loser
         else:
             raise ValueError('Unlikely 50/50 split')
         
-        votes_total += red_votes
-        votes_total += blue_votes
-    
-    print(int(votes_total), 'total votes')
+    print(int(election_votes), 'total votes')
     print(int(wasted_red), 'wasted red votes')
     print(int(wasted_blue), 'wasted blue votes')
     
     if wasted_blue > wasted_red:
-        print('+{:.1f}% gap for red team'.format(100 * (wasted_blue - wasted_red) / votes_total))
+        print('+{:.1f}% gap for red team'.format(100 * (wasted_blue - wasted_red) / election_votes))
     else:
-        print('+{:.1f}% gap for blue team'.format(100 * (wasted_red - wasted_blue) / votes_total))
+        print('+{:.1f}% gap for blue team'.format(100 * (wasted_red - wasted_blue) / election_votes))
 
 if __name__ == '__main__':
     _, plan_path = sys.argv
