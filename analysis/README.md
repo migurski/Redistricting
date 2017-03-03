0b4c3487ced46342dce2df14933faf088b8cc993
Michal Migurski <mike@teczno.com>
2017-03-02 23:21:36 -0800

    Recalculated gaps using Pres./House adjustments

--- analysis/README.md
+++ analysis/README.md
@@ -1,23 +1,47 @@
 Gaps
 ====
 
-U.S. House (53 seats)
+### U.S. House (53 seats)
 
+<!--
             Actual      Predicted   Olson
-    Dem     38          46 (+8)     42 (+4)
-    Rep     14          7           11
-    gap     R +4.5%     D +4.0%     R +2.2%
+    Dem     38          45 (+8)     41 (+3)
+    Rep     14          8           12
+    gap     R +0.3%     D +8.6%     D +2.0%
+-->
+<table>
+    <tr><th></th><th>Actual</th><th>Predicted</th><th>Olson</th></tr>
+    <tr><td>Dem</td><td>38</td><td>45 (+8)</td><td>41 (+3)</td></tr>
+    <tr><td>Rep</td><td>14</td><td>8</td><td>12</td></tr>
+    <tr><td>gap</td><td>R +0.3%</td><td>D +8.6%</td><td>D +2.0%</td></tr>
+</table>
 
-CA State Senate (40 seats)
+### CA State Senate (40 seats)
 
+<!--
             Actual      Predicted   Olson
-    Dem     27          33 (+6)     34 (+7)
-    Rep     13          7           6
-    gap                 R +1.1%     D +2.6%
+    Dem     27          31 (+4)     34 (+7)
+    Rep     13          9           6
+    gap                 D +0.5%     D +9.7%
+-->
+<table>
+    <tr><th></th><th>Actual</th><th>Predicted</th><th>Olson</th></tr>
+    <tr><td>Dem</td><td>27</td><td>31 (+4)</td><td>34 (+7)</td></tr>
+    <tr><td>Rep</td><td>13</td><td>9</td><td>6</td></tr>
+    <tr><td>gap</td><td></td><td>D +0.5%</td><td>D +9.7%</td></tr>
+</table>
 
-CA State Assembly (80 seats)
+### CA State Assembly (80 seats)
 
+<!--
             Actual      Predicted   Olson
-    Dem     55          66 (+11)    68 (+13)
-    Rep     25          14          12
-    gap                 R +0.2%     D +3.1%
+    Dem     55          66 (+11)    69 (+14)
+    Rep     25          14          11
+    gap                 D +5.7%     D +9.8%
+-->
+<table>
+    <tr><th></th><th>Actual</th><th>Predicted</th><th>Olson</th></tr>
+    <tr><td>Dem</td><td>55</td><td>66 (+11)</td><td>69 (+14)</td></tr>
+    <tr><td>Rep</td><td>25</td><td>14</td><td>11</td></tr>
+    <tr><td>gap</td><td></td><td>D +5.7%</td><td>D +9.8%</td></tr>
+</table>
--- analysis/gap.py
+++ analysis/gap.py
@@ -1,29 +1,30 @@
 from osgeo import ogr
 import sys
 
-def main(plan_path):
+def calc(districts):
     '''
     '''
-    plan_ds = ogr.Open(plan_path)
-    plan_lyr = plan_ds.GetLayer(0)
+    election_votes, wasted_red, wasted_blue, red_wins, blue_wins = 0, 0, 0, 0, 0
     
-    election_votes, wasted_red, wasted_blue = 0, 0, 0
-    
-    for feature in plan_lyr:
-        red_votes, blue_votes = feature.GetField('pres_red'), feature.GetField('pres_blue')
+    for district in districts:
+        red_votes = [float(v) for (k, v) in district.items() if 'red' in k.lower()][0]
+        blue_votes = [float(v) for (k, v) in district.items() if 'blu' in k.lower()][0]
         district_votes = red_votes + blue_votes
         election_votes += district_votes
         win_threshold = district_votes / 2
     
         if red_votes > blue_votes:
+            red_wins += 1
             wasted_red += red_votes - win_threshold # surplus
             wasted_blue += blue_votes # loser
         elif blue_votes > red_votes:
+            blue_wins += 1
             wasted_blue += blue_votes - win_threshold # surplus
             wasted_red += red_votes # loser
         else:
             raise ValueError('Unlikely 50/50 split')
         
+    print(int(blue_wins), 'blue and', int(red_wins), 'red districts')
     print(int(election_votes), 'total votes')
     print(int(wasted_red), 'wasted red votes')
     print(int(wasted_blue), 'wasted blue votes')
@@ -32,6 +33,18 @@ def main(plan_path):
         print('+{:.1f}% gap for red team'.format(100 * (wasted_blue - wasted_red) / election_votes))
     else:
         print('+{:.1f}% gap for blue team'.format(100 * (wasted_red - wasted_blue) / election_votes))
+
+def main(plan_path):
+    '''
+    '''
+    plan_ds = ogr.Open(plan_path)
+    plan_lyr = plan_ds.GetLayer(0)
+    
+    defn = plan_lyr.GetLayerDefn()
+    names = {defn.GetFieldDefn(i).name for i in range(defn.GetFieldCount())}
+    districts = [{n: feat.GetField(n) for n in names} for feat in plan_lyr]
+    
+    return calc(districts)
 
 if __name__ == '__main__':
     _, plan_path = sys.argv
