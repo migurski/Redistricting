0517beb889968c6edd11eeaacca6682dd9048084
Michal Migurski <mike@teczno.com>
2017-03-21 23:20:05 -0700

    Started generating graphs from database to include populations

--- 55 - Wisconsin/shapefile-graph.py
+++ 55 - Wisconsin/shapefile-graph.py
@@ -1,48 +1,44 @@
 #!/usr/bin/env python3
 ''' Generate a graph from neighboring census tracts.
 '''
-import sys, itertools, networkx
-from osgeo import ogr
+import sys, networkx, psycopg2
 
-def get_multilinestring(geometry):
-    '''
-    '''
-    if geometry.GetGeometryType() in (ogr.wkbLineString, ogr.wkbMultiLineString):
-        return geometry
-    elif geometry.GetGeometryType() == ogr.wkbGeometryCollection:
-        multiline = ogr.Geometry(ogr.wkbMultiLineString)
-        for geom in geometry:
-            if geom.GetGeometryType() == ogr.wkbLineString:
-                multiline.AddGeometry(geom)
-        return multiline
-    else:
-        return None
+_, postgres_dsn, table_name, graph_path = sys.argv
 
-_, shp_path, graph_path = sys.argv
-
-ds = ogr.Open(shp_path)
-tracts = list(ds.GetLayer(0))
 graph = networkx.Graph()
+networkx.readwrite.write_gml(graph, graph_path)
 
-networkx.readwrite.write_edgelist(graph, graph_path)
+with psycopg2.connect('postgres://localhost/redistrict_wisconsin') as conn:
+    with conn.cursor() as db:
+        if table_name == 'tl_2014_55_county':
+            prefix = '05000US'
+        elif table_name == 'tl_2014_55_tract':
+            prefix = '14000US'
+        else:
+            raise ValueError(table_name)
+    
+        print('Selecting nodes...', file=sys.stderr)
+        db.execute('''select g."GEOID", c."B02001001"
+                      from {table} as g, acs2015_5yr_b02001 as c
+                      where c.geoid = %s||g."GEOID"
+                   '''.format(table=table_name), (prefix, ))
+        
+        for (geoid, population) in db.fetchall():
+            graph.add_node(geoid, population=population)
 
-for (tract1, tract2) in itertools.combinations(tracts, 2):
-    geom1 = tract1.GetGeometryRef()
-    geom2 = tract2.GetGeometryRef()
+        print('Found', len(graph.nodes()), 'nodes', file=sys.stderr)
 
-    if not geom1.Touches(geom2):
-        continue
-    
-    intersection = geom1.Intersection(geom2)
-    geometry = get_multilinestring(intersection)
-    
-    if geometry is None:
-        continue
-    
-    graph.add_edge(tracts.index(tract1), tracts.index(tract2))
-    
-    print(tracts.index(tract1), tract1.GetField('GEOID'),
-          tracts.index(tract2), tract2.GetField('GEOID'),
-          file=sys.stderr)
+        print('Selecting edges...', file=sys.stderr)
+        db.execute('''select c1."GEOID", c2."GEOID"
+                      from {table} as c1, {table} as c2
+                      where c1."GEOID" < c2."GEOID"
+                        and ST_Touches(c1.geom, c2.geom)
+                        and ST_GeometryType(ST_Intersection(c1.geom, c2.geom)) != 'ST_Point'
+                   '''.format(table=table_name))
+        
+        for (geoid1, geoid2) in db.fetchall():
+            graph.add_edge(geoid1, geoid2)
 
-networkx.readwrite.write_edgelist(graph, graph_path)
+        print('Found', len(graph.edges()), 'edges', file=sys.stderr)
+
+networkx.readwrite.write_gml(graph, graph_path)
