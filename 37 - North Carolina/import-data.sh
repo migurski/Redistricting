226f8dfad090e9f4a5a2ae0c5921e4f942f8eb9d
Michal Migurski <mike@teczno.com>
2017-03-30 19:06:08 -0700

    Fixed district detection in the big query

--- 37 - North Carolina/import-data.sh
+++ 37 - North Carolina/import-data.sh
@@ -21,7 +21,7 @@ votes as (
     select parent_jurisdiction as county_name, jurisdiction as precinct_id,
         (case
          when office = 'US SENATE' then 'senate'
-         when office = 'US HOUSE OF REPRESENTATIVES' then 'uscd'
+         when office = 'US HOUSE OF REPRESENTATIVES' then 'congress'
          when office = 'NC STATE SENATE' then 'sldu'
          when office = 'NC HOUSE OF REPRESENTATIVES' then 'sldl'
          else null end) as office,
@@ -35,10 +35,19 @@ votes as (
     group by parent_jurisdiction, jurisdiction, office, district, party
 )
 select distinct s.shape_id, v.county_name, v.precinct_id,
+    -- U.S. Senate has no district
     sum(sen_red.votes) as sen_red, sum(sen_blu.votes) as sen_blue,
-    string_agg(distinct uscd_red.district, ',' ORDER BY uscd_red.district) as uscd_districts, sum(uscd_red.votes) as uscd_red, sum(uscd_blu.votes) as uscd_blue,
-    string_agg(distinct sldu_red.district, ',' ORDER BY sldu_red.district) as sldu_districts, sum(sldu_red.votes) as sldu_red, sum(sldu_blu.votes) as sldu_blue,
-    string_agg(distinct sldl_red.district, ',' ORDER BY sldl_red.district) as sldl_districts, sum(sldl_red.votes) as sldl_red, sum(sldl_blu.votes) as sldl_blue
+    -- Use string_agg on Congressional and State Leg. districts,
+    -- because one precinct sometimes votes for different districts.
+    string_agg(distinct con_red.district, ',' ORDER BY con_red.district) as con_red_districts,
+    string_agg(distinct con_blu.district, ',' ORDER BY con_blu.district) as con_blue_districts,
+    sum(con_red.votes) as con_red_votes, sum(con_blu.votes) as con_blue_votes,
+    string_agg(distinct sldu_red.district, ',' ORDER BY sldu_red.district) as sldu_red_districts,
+    string_agg(distinct sldu_blu.district, ',' ORDER BY sldu_blu.district) as sldu_blue_districts,
+    sum(sldu_red.votes) as sldu_red_votes, sum(sldu_blu.votes) as sldu_blue_votes,
+    string_agg(distinct sldl_red.district, ',' ORDER BY sldl_red.district) as sldl_red_districts,
+    string_agg(distinct sldl_blu.district, ',' ORDER BY sldl_blu.district) as sldl_blue_districts,
+    sum(sldl_red.votes) as sldl_red_votes, sum(sldl_blu.votes) as sldl_blue_votes
 from shapes as s
 left join votes as v
   on v.county_name = s.county_name and v.precinct_id = s.precinct_id
@@ -46,10 +55,10 @@ left join votes as sen_red
   on sen_red.county_name = v.county_name and sen_red.precinct_id = v.precinct_id and sen_red.office = 'senate' and sen_red.party = 'red'
 left join votes as sen_blu
   on sen_blu.county_name = v.county_name and sen_blu.precinct_id = v.precinct_id and sen_blu.office = 'senate' and sen_blu.party = 'blue'
-left join votes as uscd_red
-  on uscd_red.county_name = v.county_name and uscd_red.precinct_id = v.precinct_id and uscd_red.office = 'uscd' and uscd_red.party = 'red'
-left join votes as uscd_blu
-  on uscd_blu.county_name = v.county_name and uscd_blu.precinct_id = v.precinct_id and uscd_blu.office = 'uscd' and uscd_blu.party = 'blue'
+left join votes as con_red
+  on con_red.county_name = v.county_name and con_red.precinct_id = v.precinct_id and con_red.office = 'congress' and con_red.party = 'red'
+left join votes as con_blu
+  on con_blu.county_name = v.county_name and con_blu.precinct_id = v.precinct_id and con_blu.office = 'congress' and con_blu.party = 'blue'
 left join votes as sldu_red
   on sldu_red.county_name = v.county_name and sldu_red.precinct_id = v.precinct_id and sldu_red.office = 'sldu' and sldu_red.party = 'red'
 left join votes as sldu_blu
